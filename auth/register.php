39effa1ed5bcff29e447b9e05c55854b7f5fdf54
projectdika <projectdika88@gmail.com>
2025-11-09 17:04:24 +0800

    menambahkan auth untuk login dan register

---
+++ auth/register.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register Page | Wira Harapan</title>
+
+    <!-- My CSS -->
+     <link rel="stylesheet" href="../css/login.css">
+
+    <!-- My Fonts  -->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome CDN -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body>
+    <div class="container">
+        <div class="form-box" id="login-form">
+            <form action="">
+                <h2>Register</h2>
+                <input type="text" name="name" placeholder="Name" required>
+                <input type="email" name="email" placeholder="Email" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <select name="role" required>
+                    <option value="">--Select Role--</option>
+                    <option value="user">Murid</option>
+                    <option value="teacher">Guru</option>
+                </select>
+                <button type="submit" name="login">Login</button>
+                <p>Already have an account? <a href="login.php">Login</a></p>
+            </form>
+        </div>
+    </div>
+    
+</body>
+</html>
